ed6f72dfb6dc9f6c270e71d4445d8922ce8fd1ba
Boje <math039rrelev.techcollege.dk>
2025-01-14 14:04:02 +0100

    Added Changed long to Integer

--- src/main/java/com/boje/h5skemaproject/repositories/SubjectRepository.java
+++ src/main/java/com/boje/h5skemaproject/repositories/SubjectRepository.java
@@ -3,5 +3,5 @@ package com.boje.h5skemaproject.repositories;
 import com.boje.h5skemaproject.models.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
 }
--- src/main/java/com/boje/h5skemaproject/repositories/TeacherRepository.java
+++ src/main/java/com/boje/h5skemaproject/repositories/TeacherRepository.java
@@ -3,5 +3,5 @@ package com.boje.h5skemaproject.repositories;
 import com.boje.h5skemaproject.models.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
 }
